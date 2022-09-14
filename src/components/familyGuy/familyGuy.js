bd3099d6118bbdfc8277426d51ed276fda1cbd7a
Orestarxx <oresto322@gmail.com>
2022-09-14 17:27:54 +0300

    comit

--- src/components/familyGuy/familyGuy.js
+++
@@ -1,9 +0,0 @@
-import {familyGuy} from "../data/data";
-import Guys from "./familyGuyBuild";
-
-
-export default function   FamilyGuys(){
-    return(<div className='familyDiv'>
-        {familyGuy.map((guy) =>(<Guys item={guy} key={guy.id}/>))}
-    </div>)
-}
--- src/components/familySimpsons/familySimpsons.js
+++
@@ -1,9 +0,0 @@
-import {simpsons} from "../data/data";
-import Simpson from "./simpsonsBuild";
-
-export default function Simpsons(){
-
-    return(<div className="holder">
-        {simpsons.map(simpson =>( <Simpson item={simpson} key={simpson.id}/>))}
-    </div>)
-}
