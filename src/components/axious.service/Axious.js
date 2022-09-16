ac0012dbd6e48c15cc285f321dd24cb7eea68b7d
Orestarxx <oresto322@gmail.com>
2022-09-16 14:03:45 +0300

    gg)

---
+++ src/components/axious.service/Axious.js
@@ -0,0 +1,19 @@
+import {useState} from "react";
+import SpaceBuild from "./SpaceXBuild";
+
+export default function SpaceX (){
+    let [spaceX,setSpaceX] = useState([])
+    fetch('https://api.spacexdata.com/v3/launches/')
+        .then(value => value.json())
+        .then(value => {
+            let result = value.filter(val =>val.launch_year !== '2020')
+            setSpaceX(result)
+
+
+
+
+        })
+    return(<div>
+        {spaceX.map((space,index)=>(<SpaceBuild item={space} key={space.index}/>))}
+    </div>)
+}
