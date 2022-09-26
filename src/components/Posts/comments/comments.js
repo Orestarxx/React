d2dad96bd70e3ac6472987eb7f9e22a82c31fa63
Orestarxx <oresto322@gmail.com>
2022-09-27 02:26:30 +0300

    big

--- src/components/Posts/comments/comments.js
+++ src/components/Posts/comments/comments.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useLocation} from "react-router-dom";
+import {commentsService} from "../../../service/comments.service";
 
 const Comments = () => {
    const location = useLocation();
     console.log(location);
-
+    const {state:commnets} = location;
+ useEffect(() =>{
+ commentsService.getAll(postId)
+ },[])
     return (
         <div>
 
