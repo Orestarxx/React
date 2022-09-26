8ade3ffe8c61c9a749908f1a44f9f93d42365a58
Orestarxx <oresto322@gmail.com>
2022-09-26 04:09:52 +0300

    comit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@
 import {BrowserRouter,Routes,Route,Link,Outlet} from 'react-router-dom'
 
 
-import {Todo} from "./components/TodosHolder/Todo/Todo";
-import {Album} from "./components/AlbumsHolder/Album/Album";
-import {Comment} from "./components/PostsCommentsHolder/Comment/Comment";
-import {PostDetailsBuild} from "./components/PostsCommentsHolder/CommentsBuild/PostDetailsBuild";
+
 
 
 
@@ -14,20 +11,6 @@ import {PostDetailsBuild} from "./components/PostsCommentsHolder/CommentsBuild/P
 function App() {
 
   return (<div>
-        <div>
-          <div><Link to={'/todos'}><b>Todos appearing</b></Link></div>
-          <div><Link to={'/albums'}><b>Albums appearing</b></Link></div>
-            <div><Link to={'/comments'}> <b>Comments appearing</b></Link></div>
-
-        </div>
-        <Routes>
-          <Route path={'todos'} element={ <Todo/>}/>
-            <Route path={'albums'} element={<Album/>}/>
-            <Route path={'comments'} element={<Comment/>}/>
-            <Route path={'posts/:id'} element={<PostDetailsBuild/>}/>
-
-
-        </Routes>
 
 
       </div>
