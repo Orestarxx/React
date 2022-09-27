cf715034a34f2c7a3baa1c17801550987907b7ed
Orestarxx <oresto322@gmail.com>
2022-09-27 04:26:41 +0300

    big

--- src/service/comments.service.js
+++ src/service/comments.service.js
@@ -1,7 +1,16 @@
-import {axiosService} from "./axious.service";
-import {baseURL, urls} from "../configs/urls";
+// import {axiosService} from "./axious.service";
+// import {baseURL, urls} from "../configs/urls";
+//
+// const commentsService ={
+//     getAll:(id) => axiosService.get(`${urls.comments}/${id}`)
+//  }
+//  export {commentsService}
 
-const commentsService ={
-    getAll:(id) => axiosService.get(`${urls.comments}/${id}`)
- }
- export {commentsService}
+import axios from "axios";
+
+let baseComments = axios.create({
+    baseURL:'https://jsonplaceholder.typicode.com'
+})
+const getAxiousComments = () =>{
+    return baseComments('/comments'+ id)
+}
