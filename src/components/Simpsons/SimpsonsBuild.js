2f5b5d289ef0eee2a34f80e11909fe93bf927f89
Orestarxx <oresto322@gmail.com>
2022-09-28 03:15:58 +0300

    big

--- src/components/Simpsons/SimpsonsBuild.js
+++ src/components/Simpsons/SimpsonsBuild.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import css from './simpsonsStyle.module.css'
 
 const SimpsonsBuild = ({simpson}) => {
     const {nameSur,description,image} = simpson;
     return (
-        <div>
+        <div className={css.simpsonsHolder}>
+
             <div>{nameSur}</div>
             <div>{description}</div>
+            <div className={css.pictureHolder}>
             <img src={image} alt="Simpsons"/>
+            </div>
+
         </div>
     );
 };
