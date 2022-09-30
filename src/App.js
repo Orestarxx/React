288be03e9e1fb5d16963e06850ff5e51398a30f0
Orestarxx <oresto322@gmail.com>
2022-10-01 02:32:57 +0300

    practicing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import './components/familyGuy/familyGuyStyle.css'
-import './components/fetches/styleRick.css'
-import './App.css';
-import Simpsons from "./components/familySimpsons/familySimpsons";
-import Rickworld from "./components/fetches/fetch";
-import FamilyGuys from "./components/familyGuy/familyGuy";
+import {useReducer} from "react";
+import "./App.css"
+import {useForm} from "react-hook-form";
+const fill = (initial) =>{
+    return [{cat:initial,dog:initial}]
+}
+const reducer = (state,action) =>{
+    console.log(action);
+    switch (action.type) {
+        case 'Cat':
+            return {...state, cat: state.cat + state}
+    }
 
+    return {...state}
 
-
+}
 
 function App() {
+const [state,dispatch] = useReducer(reducer,[{cats:{},dogs:{}}],fill)
 
-  return (<div>
 
-          <Simpsons/>
-          <Rickworld/>
-          <FamilyGuys/>
+    return (<div>
+            <div className='formHolder'>
+            <form onSubmit={() => dispatch({type:'Cat'})}>
+                <input type="text"/>
+                <button>save dog</button>
+            </form>
+            <form>
+                <input type="text"/>
+                <button >Save Cat</button>
+            </form>
+            </div>
+            <div className='petsHolder'>
+            <div className='pets'>{state.cat}</div>
+            <div className='pets'>{state.dog}</div>
+
+            </div>
 
       </div>
 
