0a9297d7f9295410f1aad444e3359c446ea39ad4
Orestarxx <oresto322@gmail.com>
2022-10-05 01:42:28 +0300

    comit

--- src/axios.service/axios.service.js
+++ src/axios.service/axios.service.js
@@ -1,6 +1,17 @@
 import axios from "axios";
-import {URL} from "../configs/urls";
+import {URL, urls} from "../configs/urls";
 
 const usersAxousService= axios.create({URL});
+
+const getUsers = () =>{
+    usersAxousService(urls.users);
+}
+const getPosts = () =>{
+    usersAxousService(urls.posts);
+}
+const getComments = () =>{
+    usersAxousService(urls.comments);
+}
+export {getPosts,getComments,getUsers};
 
 
